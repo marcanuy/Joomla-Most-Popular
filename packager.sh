8e8d0fa96dbd01d2a50f48d820978ad529a7efbd
Marcelo Canina <me@marcanuy.com>
2014-01-22 17:42:30 +0000

    packager filename version

--- packager.sh
+++ packager.sh
@@ -2,4 +2,4 @@
 VERSION=$(grep -oP '(?<=<version>).*?(?=</version>)' pkg_mostpopular.xml)
 zip -r packages/mod_articles_mostpopular.zip packages/mod_articles_mostpopular/
 zip -r packages/plg_content_mostpopular.zip packages/plg_content_mostpopular/
-zip -r pkg_mostpopular-$VERSION.zip *
+zip -r pkg_mostpopular-v$VERSION.zip *
