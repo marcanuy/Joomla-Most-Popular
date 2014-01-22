ffa2ee15ec236328d5d4fcf13a91bd951a9550d1
Marcelo Canina <me@marcanuy.com>
2014-01-22 17:22:56 +0000

    Added packager script; fixed version

---
+++ packager.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+VERSION=$(grep -oP '(?<=<version>).*?(?=</version>)' pkg_mostpopular.xml)
+zip -r packages/mod_articles_mostpopular.zip packages/mod_articles_mostpopular/
+zip -r packages/plg_content_mostpopular.zip packages/plg_content_mostpopular/
+zip -r pkg_mostpopular-$VERSION.zip *
